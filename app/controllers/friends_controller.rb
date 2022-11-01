040217415c7d6b81003576bdfc1cb9fabafbf9a8
TakumiSuzuki <ne211228@senshu-u.jp>
2022-11-01 12:04:04 +0000

    “friendレコードの作成、更新”

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,41 +3,35 @@ class FriendsController < ApplicationController
     @friends = Friend.all
   end
 
-  # def search
-  #   @members = Member.search(params[:q])
-  #   render "index"
-  # end
+  def show
+  end
 
-  # def show
-  #   @member = Member.find(params[:id])
-  # end
+  def new
+    @friend = Friend.new()
+  end
 
-  # def new
-  #   @member = Member.new(birthday: Date.new(1980, 1, 1))
-  # end
+  def edit
+    @friend = Friend.find(params[:id])
+  end
 
-  # def edit
-  #   @member = Member.find(params[:id])
-  # end
+  def create
+    @friend = Friend.new(params.require(:friend).permit(:name))
+    if @friend.save
+      redirect_to friends_path, notice: "会員を登録しました。"
+    else
+      render "new"
+    end
+  end
 
-  # def create
-  #   @member = Member.new(params[:member])
-  #   if @member.save
-  #     redirect_to @member, notice: "会員を登録しました。"
-  #   else
-  #     render "new"
-  #   end
-  # end
-
-  # def update
-  #   @member = Member.find(params[:id])
-  #   @member.assign_attributes(params[:member])
-  #   if @member.save
-  #     redirect_to @member, notice: "会員情報を更新しました。"
-  #   else
-  #     render "edit"
-  #   end
-  # end
+  def update
+    @friend = Friend.find(params[:id])
+    @friend.assign_attributes(params.require(:friend).permit(:name))
+    if @friend.save
+      redirect_to friends_path, notice: "会員情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
 
   # def destroy
   #   @member = Member.find(params[:id])
